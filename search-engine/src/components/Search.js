8ad360f771ae5042f02987454c0f6a21f09b9ee7
meghathakur23 <meghnathakur484@gmail.com>
2021-01-19 23:56:57 +0530

    minor changes

--- search-engine/src/components/Search.js
+++ search-engine/src/components/Search.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Search.css";
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
 
 function Search (){
+    const [input, setInput] = useState();
+
+    const search = (e) => {
+        e.preventDefault();
+        console.log("you hit the buttton ", input)
+    };
+
     return (
-        <div className="search">
+        <form className="search">
             <div className="search__input">
                 <SearchIcon className="searchicon"/>
-                <input placeholder="Search Google or type a URL "/>
+                <input  value={input} onChange={e => setInput(e.target.value)} placeholder="Search Google or type a URL "/>
                 <MicIcon/>
-
+            </div>
+            <div className="search_buttons">
+                <Button type="submit" onclick={search} variant="outlined">Google Search</Button>
+                <Button variant="outlined">I' m Feeling lucky</Button>
 
             </div>
-        </div>
+        </form>
     )
 }
 
